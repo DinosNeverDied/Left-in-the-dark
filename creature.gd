5fdd220b1c1d719bf669ad725fb9e60f82c45ba8
Bernges <144256150+Bernges@users.noreply.github.com>
2026-02-15 03:01:38 +0100

    Add files via upload
    updated boars, fixed aggro

---
+++ creature.gd
@@ -0,0 +1,47 @@
+class_name Creature
+extends CharacterBody2D
+
+@export var DAMAGE = 1
+@export var HEALTH = 3
+@export var WALK_SPEED = 60.0
+@export var RUN_SPEED = 120.0
+@export var IDLE_TIME = 1.5
+@export var KNOCKBACK_FORCE = 220.0
+@export var WEIGHT = 80.0
+
+@onready var pivot: Node2D = $Pivot
+@onready var animated_sprite: AnimatedSprite2D = $Pivot/AnimatedSprite2D
+
+var facing_right = true
+
+var direction: int:
+	get:
+		return 1 if facing_right else -1
+
+var momentum: int:
+	get:
+		return RUN_SPEED * WEIGHT
+
+func receive_damage(damage: int):
+	print(name, " HP: ", HEALTH, " => ", HEALTH - damage)
+	HEALTH -= damage
+	if HEALTH <= 0:
+		die()
+
+
+func receive_knockback(knockback_momentum: float):
+	# print(name, " was blocked by " + creature.name)
+	position += Vector2(knockback_momentum / WEIGHT * -direction, 0)
+
+func _physics_process(delta: float):
+	pivot.scale.x = direction
+
+	# apply gravity 
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	move_and_slide()
+
+func die():
+	print(name, " died")
+	queue_free()
--- enemy.gd
+++ enemy.gd
@@ -1,15 +1,11 @@
 class_name Enemy
 extends Creature
 
-enum State { IDLE, WALK, RUN, HIT, STUNNED }
+enum State {IDLE, WALK, RUN, HIT, STUNNED, DEAD}
 
-@export var IDLE_TIME = 2.0
-@export var WALK_SPEED = 60.0
-@export var KNOCKBACK_FORCE = 220.0
 @export var STUN_TIME = 0.4
 
-@onready var ray_cast: RayCast2D = $RayCast2D
-@onready var ray_cast2: RayCast2D = $RayCast2D2
+@onready var raycast: RayCast2D = $Pivot/RayCast
 @onready var aggro: Area2D = $Aggro
 @onready var hitbox: Area2D = $Hitbox
 
@@ -17,7 +13,7 @@ var state: State = State.WALK
 var idle_timer = 0.0
 var stun_timer = STUN_TIME
 var knockback_velocity = Vector2.ZERO
-var player_shape: Node2D = null
+var player: Node2D = null
 
 func _physics_process(delta: float) -> void:
 	
@@ -32,22 +28,24 @@ func _physics_process(delta: float) -> void:
 			handle_stunned(delta)
 		State.HIT:
 			pass
+		State.DEAD:
+			return
 				
-	move_and_slide()
+	super._physics_process(delta)
 	
-
-func handle_walk(_delta: float) -> void:
+func _ready() -> void:
+	print("facing_right: ", facing_right)
 	
-	#change direction according to which raycast is colliding
-	#idle after hitting an edge
-	if ray_cast.is_colliding():
-		facing_right = false
+func handle_walk(delta: float) -> void:
+	
+	if raycast.is_colliding():
 		state = State.IDLE
-	elif ray_cast2.is_colliding():
-		facing_right = true
-		state = State.IDLE
-		
+		facing_right = not facing_right
+		print("Ray cast front colliding. facing_right = ", facing_right)
+			
 	if is_on_floor():
+		#("direction: " , direction)
+		#print("WALK_SPEED: " , WALK_SPEED)
 		velocity.x = direction * WALK_SPEED
 		animated_sprite.play("walk")
 
@@ -65,15 +63,15 @@ func handle_idle(delta: float) -> void:
 
 func handle_run() -> void:
 	
-	if not player_shape:
+	if not player:
 		state = State.WALK
 		return
 	
-	direction = sign(player_shape.global_position.x - global_position.x)
+	facing_right = sign(player.global_position.x - global_position.x) > 0
 	velocity.x = direction * RUN_SPEED
 	animated_sprite.play("run")
-	
 
+	
 func handle_stunned(delta: float) -> void:
 	
 	stun_timer -= delta
@@ -81,17 +79,54 @@ func handle_stunned(delta: float) -> void:
 	velocity.x = knockback_velocity.x
 	knockback_velocity = knockback_velocity.move_toward(Vector2.ZERO, 800 * delta)
 	animated_sprite.play("knockback")
+
 	
 	if stun_timer <= 0:
 		stun_timer = STUN_TIME
-		state = State.RUN if player_shape != null else State.WALK
+		state = State.RUN if player != null else State.WALK
 
 
-func _on_aggro_body_entered(body: CollisionShape2D):
-	player_shape = body
-	state = State.RUN
+func _on_aggro_body_entered(body: Node2D):
+	
+	if body.is_in_group("player"):
+		player = body
+		state = State.RUN
 		
+		
+func _on_aggro_body_exited(body: Node2D):
+	
+	if body == player:
+		player = null
+		state = State.WALK
+		
+		
+func _on_hitbox_body_entered(body: Node2D):
+	if body.is_in_group("player_attack"):
+		take_damage(1)
+	elif body.is_in_group("player_block"):
+		apply_blocked_knockback(body)
+		
+		
+func apply_blocked_knockback(source: Node2D):
+	state = State.STUNNED
+	stun_timer = STUN_TIME
+	
+	var knock_dir = sign(global_position.x - source.global_position.x)
+	direction = knock_dir
+	
+	knockback_velocity = Vector2(
+		knock_dir * KNOCKBACK_FORCE,
+		-40
+	)
+	animated_sprite.play("knockback")
 
-func _on_aggro_body_exited(_body: CollisionShape2D):
-	player_shape = null
-	state = State.WALK
+
+func take_damage(amount: int):
+	
+	HEALTH -= amount
+	animated_sprite.play("hit")
+	state = State.HIT
+	
+	if HEALTH <= 0:
+		die()
+		
